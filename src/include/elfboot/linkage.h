ea63e4884840bd0f9b46ba58debbc9ff4dc51c3b
Christopher Roemheld <chris.roemheld@tum.de>
2019-07-15 20:36:55 +0200

    Place real mode functions in separate sections

--- src/include/elfboot/linkage.h
+++ src/include/elfboot/linkage.h
@@ -5,11 +5,32 @@
 
 #include <asm/linkage.h>
 
+/*
+ * Sections
+ */
+
 #define __section(x)			__attribute__((section(x)))
 
+/*
+ * Macro to place real mode stubs at the beginning
+ * of the generated binary file in order to ensure
+ * that it is accessible in real mode.
+ *
+ * Only neccessary if bootloader oversteps the max
+ * size allowed (within 64KB).
+ */
+#define __real_function			__section(".real.functions")
+
+/*
+ * Structures and functions
+ */
 
 #define __unused			__attribute__((unused))
 #define __packed			__attribute__((packed))
+
+/*
+ * Alignment
+ */
 
 #define __aligned(x)			__attribute__((aligned(x)))
 
