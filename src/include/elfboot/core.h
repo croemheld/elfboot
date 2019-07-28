0c635bc27991b591d305c1a486b33f1f6a75b82a
Christopher Roemheld <chris.roemheld@tum.de>
2019-07-28 12:44:55 +0200

    Fix for aesthethic reasons

--- src/include/elfboot/core.h
+++ src/include/elfboot/core.h
@@ -11,7 +11,7 @@
 
 #define __round_mask(x, y)                ((__typeof__(x))((y) - 1))
 #define round_up(x, y)                    ((((x) - 1) | __round_mask(x, y)) + 1)
-#define round_down(x, y)                  ((x) & ~__round_mask(x, y))
+#define round_down(x, y)                  ((x) & ~ __round_mask(x, y))
 
 #define ARRAY_SIZE(x)                     (sizeof(x) / sizeof(*(x)))
 
