fd0dc374d9ecbd717a73549fb9b4e7486dda0d8e
Christopher Roemheld <chris.roemheld@tum.de>
2019-09-10 09:36:54 +0200

    Add separate initialization function

--- src/arch/x86/include/asm/boot.h
+++ src/arch/x86/include/asm/boot.h
@@ -40,7 +40,10 @@ void prepare_kernel(struct boot_params *boot_params);
 /* Jump into our cr0S kernel */
 void kernel_init(void);
 
+/* Architecture-specific main function*/
+int arch_init_late(void);
 int arch_main(uint8_t disk_drive);
+
 #endif /* __ASSEMBLER__ */
 
 #endif /* __X86_BOOT_H__ */
