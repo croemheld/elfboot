485cf12061039cff60e3f63276ec91e8028e8b24
Christopher Roemheld <chris.roemheld@tum.de>
2019-09-10 09:36:26 +0200

    Add bochs debugging functions

--- src/include/bochs.h
+++ src/include/bochs.h
@@ -1,9 +1,27 @@
 #ifndef __UAPI_ELFBOOT_DEBUG_H__
 #define __UAPI_ELFBOOT_DEBUG_H__
 
-static inline void bochs_bp(void)
+#include <elfboot/core.h>
+#include <elfboot/linkage.h>
+#include <elfboot/io.h>
+
+static __always_inline void bochs_bp(void)
 {
-	asm volatile("xchg %bx, %bx");
+	outw(0x8A00, 0x8A00);
+	outw(0x8A00, 0x8AE0);
+}
+
+static __always_inline void bochs_putc(char c)
+{
+	outb(0xE9, c);
+}
+
+static __always_inline void bochs_bprintf(const char *buffer, int length)
+{
+	int i;
+
+	for (i = 0; i < length; i++)
+		bochs_putc(buffer[i]);
 }
 
 #endif /* __UAPI_ELFBOOT_DEBUG_H__ */
