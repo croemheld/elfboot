6cab6741254eccc4e778e6a13d6734bc6d11d8d4
Christopher Roemheld <chris.roemheld@tum.de>
2020-04-25 20:37:39 +0200

    Fix return value from ffs

--- src/arch/x86/include/asm/bitops.h
+++ src/arch/x86/include/asm/bitops.h
@@ -9,13 +9,13 @@ static inline int ffs(uint32_t val)
 
 	asm(
 		"bsfl    %1, %0		\n\t"
-		"jnz     1f		\n\t"
+		"jnz     1f			\n\t"
 		"movl    $-1, %0	\n"
 
 		"1:" : "=r" (r) : "rm" (val)
 	);
 
-	return r + 1;
+	return r;
 }
 
 static inline int fls(uint32_t val)
@@ -24,7 +24,7 @@ static inline int fls(uint32_t val)
 
 	asm(
 		"bsrl    %1, %0		\n\t"
-		"jnz     1f		\n\t"
+		"jnz     1f			\n\t"
 		"movl    $-1, %0	\n"
 
 		"1:" : "=r" (r) : "rm" (val)
