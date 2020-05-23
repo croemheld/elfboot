3cc129b8522124ab942eafd9be9eade7050b3157
Christopher Roemheld <chris.roemheld@tum.de>
2020-05-23 23:45:32 +0200

    Use more flexible linkage macros

--- src/arch/x86/include/asm/linkage.h
+++ src/arch/x86/include/asm/linkage.h
@@ -3,12 +3,19 @@
 
 #ifdef __ASSEMBLER__
 
-#define LOCAL(name)				\
+#define SYM_L_GLOBAL(name)	.global name
+#define SYM_L_LOCAL(name)	/* Nothing */
+
+#define ALIGN(align)		.p2align (align), 0x90
+
+#define SYMBOL(name, linkage, align)	\
+	ALIGN(align);						\
+	linkage(name);						\
 name:
 
-#define GLOBAL(name)			\
-    .global name;				\
-name:
+#define LOCAL(name)		SYMBOL(name, SYM_L_LOCAL,  0)
+
+#define GLOBAL(name)	SYMBOL(name, SYM_L_GLOBAL, 0)
 
 #define ENDPROC(name)			\
     .type name, @function;		\
