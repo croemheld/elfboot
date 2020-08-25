e48f8c327c34aa26c5dc8f4431c8ad848b44a277
Christopher Roemheld <chris.roemheld@tum.de>
2020-08-25 04:13:28 +0200

    Add more console attributes, extend TTY driver

--- src/drivers/char/tty.c
+++ src/drivers/char/tty.c
@@ -6,42 +6,43 @@
 #include <elfboot/console.h>
 #include <elfboot/file.h>
 #include <elfboot/cdev.h>
+#include <elfboot/math.h>
 #include <elfboot/string.h>
 #include <elfboot/printf.h>
+#include <elfboot/debug.h>
+#include <elfboot/libtmg.h>
 
 #include <drivers/tty.h>
+
+#include <uapi/elfboot/ioctls.h>
+
+#define VGA_COLOR(fgcolor, bgcolor)	\
+	(fgcolor).vga.color | (bgcolor).vga.color << 4
 
 /*
  * Current coordinates in the TTY
  */
 static struct console tty_console = {
+	.active = 1,
 	.fbaddr = TTY_BASE_ADDRESS,
 	.width  = TTY_MAX_WIDTH,
 	.height = TTY_MAX_HEIGHT,
 	.xunit  = 1,
 	.yunit  = 1,
-	.bpu = 2
+	.bpu = 2,
+	.fgcolor = COLOR_VGA(COLOR_VGA_GRAY),
+	.bgcolor = COLOR_VGA(COLOR_VGA_BLACK)
 };
 
-static int tty_read(struct cdev *cdev, uint64_t offset, uint64_t length,
-	void *buffer)
+/*
+ * TTY utilities
+ */
+
+static uint16_t tty_char(struct console *cons, char c)
 {
-	return -ENOTSUP;
-}
+	uint16_t color = VGA_COLOR(cons->fgcolor, cons->bgcolor);
 
-static uint16_t tty_char(char c)
-{
-	return (uint16_t)c | (0x07 << 8);
-}
-
-static void tty_clear(struct console *cons)
-{
-	uint16_t *dst, val;
-
-	dst = console_line(cons, 0);
-	val = tty_char(CONSOLE_CHAR_SPACE);
-
-	memset16(dst, val, console_bpl(cons) * TTY_MAX_HEIGHT);
+	return (uint16_t)c | color << 8;
 }
 
 static void tty_cursor_update(struct console *cons)
@@ -52,6 +53,99 @@ static void tty_cursor_update(struct console *cons)
 	outb(0x3D5, (lpos >> 0) & 0xFF);
 	outb(0x3D4, 0x0E);
 	outb(0x3D5, (lpos >> 8) & 0xFF);
+}
+
+static void tty_cursor_enable(struct console *cons)
+{
+	outb(0x3D4, 0x0A);
+	outb(0x3D5, (inb(0x3D5) & 0xC0) | 0x0E);
+	outb(0x3D4, 0x0B);
+	outb(0x3D5, (inb(0x3D5) & 0xE0) | 0x0F);
+
+	tty_cursor_update(cons);
+
+	cons->active = 1;
+}
+
+static void tty_cursor_disable(struct console *cons)
+{
+	tty_cursor_update(cons);
+
+	outb(0x3D4, 0x0A);
+	outb(0x3D5, 0x20);
+
+	cons->active = 0;
+}
+
+/*
+ * TTY IOCTLs
+ */
+
+static void tty_paint(struct console *cons, struct tmg_header *tmg)
+{
+	uint16_t xpos, ypos;
+	uint16_t maxw, maxh;
+	uint16_t word, *plot, *unit;
+
+	maxw = min(TTY_MAX_WIDTH, (cons->xpos + tmg->width));
+	maxh = min(TTY_MAX_HEIGHT, cons->ypos + tmg->height);
+
+	for (ypos = 0; cons->ypos + ypos < maxh; ypos += 2) {
+		for (xpos = 0; cons->xpos + xpos < maxw; xpos++) {
+			/*
+			 * Every time we want to print something, we have to fetch the plot
+			 * (the encoded character) from the file for which we are forced to
+			 * move the pointer in the file.
+			 */
+			word = sizeof(*tmg) + (ypos * tmg->width) + (xpos * cons->bpu);
+			plot = vptradd(tmg, word);
+			unit = console_unit(cons, cons->xpos + xpos, cons->ypos + ypos / 2);
+			memset16(unit, *plot, 2);
+		}
+	}
+}
+
+static void tty_reset(struct console *cons, struct console_attr *attr)
+{
+	if (!attr)
+		return;
+
+	cons->xpos = attr->xpos;
+	cons->ypos = attr->ypos;
+
+	cons->fgcolor = attr->fgcolor;
+	cons->bgcolor = attr->bgcolor;
+
+	if (attr->active)
+		tty_cursor_enable(cons);
+}
+
+static void tty_clean(struct console *cons, struct console_attr *attr)
+{
+	uint16_t *dst, val;
+
+	tty_reset(cons, attr);
+
+	dst = console_line(cons, 0);
+	val = tty_char(cons, CONSOLE_CHAR_SPACE);
+
+	memset16(dst, val, console_bpl(cons) * TTY_MAX_HEIGHT);
+
+	if (!attr)
+		tty_cursor_disable(cons);
+	else
+		tty_reset(cons, attr);
+}
+
+static void tty_gattr(struct console *cons, struct console_attr *attr)
+{
+	attr->active = cons->active;
+	attr->width  = cons->width;
+	attr->height = cons->height;
+	attr->xpos = cons->xpos;
+	attr->ypos = cons->ypos;
+	attr->fgcolor = cons->fgcolor;
+	attr->bgcolor = cons->bgcolor;
 }
 
 static void tty_scroll(struct console *cons, int direction, int units)
@@ -68,10 +162,17 @@ static void tty_scroll(struct console *cons, int direction, int units)
 
 	dst = console_line(cons, cons->height - units);
 
-	memset16(dst, tty_char(CONSOLE_CHAR_SPACE), console_bpl(cons) * units);
+	memset16(dst, tty_char(cons, CONSOLE_CHAR_SPACE), console_bpl(cons) * units);
 }
 
-static int __tty_write(struct console *cons, uint64_t length, const char *buffer)
+static int tty_read(struct cdev *cdev, uint64_t offset, uint64_t length,
+	void *buffer)
+{
+	return -ENOTSUP;
+}
+
+static int __tty_write(struct console *cons, uint64_t offset, uint64_t length,
+	const char *buffer)
 {
 	uint64_t index;
 	uint16_t *unit;
@@ -83,15 +184,18 @@ static int __tty_write(struct console *cons, uint64_t length, const char *buffer
 			cons->ypos -= cons->yunit;
 		}
 
-		tty_cursor_update(cons);
+		if (cons->active)
+			tty_cursor_update(cons);
 
 		/* Handle different characters */
-		if (buffer[index] == CONSOLE_CHAR_NEWLINE) {
+		if (buffer[index] == CONSOLE_CHAR_CRETURN)
+			cons->xpos  = 0;
+		else if (buffer[index] == CONSOLE_CHAR_NEWLINE) {
 			cons->xpos  = 0;
 			cons->ypos += cons->yunit;
 		} else if (buffer[index] != CONSOLE_CHAR_CRETURN) {
 			unit = console_unit(cons, cons->xpos++, cons->ypos);
-			memset16(unit, tty_char(buffer[index]), 1);
+			memset16(unit, tty_char(cons, buffer[index]), 2);
 
 			if (cons->xpos >= TTY_MAX_WIDTH) {
 				cons->xpos = 0;
@@ -106,11 +210,33 @@ static int __tty_write(struct console *cons, uint64_t length, const char *buffer
 static int tty_write(struct cdev *cdev, uint64_t offset, uint64_t length,
 	const void *buffer)
 {
-	return __tty_write(cdev->private, length, buffer);
+	struct console *cons = cdev->private;
+
+	if (offset)
+		cons->ypos = div_u64_rem(offset, TTY_MAX_WIDTH, &cons->xpos);
+
+	return __tty_write(cons, offset, length, buffer);
 }
 
 static int tty_ioctl(struct cdev *cdev, int request, void *args)
 {
+	switch (request) {
+		case IOCTL_RESET:
+			tty_reset(cdev->private, args);
+			break;
+		case IOCTL_CLEAN:
+			tty_clean(cdev->private, args);
+			break;
+		case IOCTL_PAINT:
+			tty_paint(cdev->private, args);
+			break;
+		case IOCTL_GATTR:
+			tty_gattr(cdev->private, args);
+			break;
+		default:
+			return -ENOTSUP;
+	}
+
 	return 0;
 }
 
@@ -137,7 +263,7 @@ static int tty_init(void)
 	if (cdev_init(cdev, &tty_cdev_ops))
 		goto tty_free_name;
 
-	tty_clear(cdev->private);
+	tty_clean(cdev->private, NULL);
 
 	return console_init("/dev/tty");
 
--- src/include/elfboot/console.h
+++ src/include/elfboot/console.h
@@ -2,16 +2,81 @@
 #define __ELFBOOT_CONSOLE_H__
 
 #include <elfboot/core.h>
-#include <elfboot/screen.h>
+#include <elfboot/input.h>
 
 #define CONSOLE_CHAR_NEWLINE	'\n'
 #define CONSOLE_CHAR_CRETURN	'\r'
 #define CONSOLE_CHAR_SPACE		' '
 
+#define CONSOLE_CTRL_ENTER		CONSOLE_CHAR_NEWLINE
+#define CONSOLE_CTRL_ARRUP		KEY_ARROW_UP
+#define CONSOLE_CTRL_ARRDO		KEY_ARROW_DOWN
+
 #define CONSOLE_SCROLL_UP		0
 #define CONSOLE_SCROLL_DOWN		1
 
+/*
+ * The struct rgb_color and struct hex_color are congruent
+ * so that it is possible to use either value at the same 
+ * time.
+ */
+
+struct rgb_color {
+#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+	uint8_t alpha;
+	uint8_t b;
+	uint8_t g;
+	uint8_t r;
+#else /* __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__*/
+	uint8_t r;
+	uint8_t g;
+	uint8_t b;
+	uint8_t alpha;
+#endif
+};
+
+struct hex_color {
+	uint32_t color;
+};
+
+/*
+ * The struct vga_color only holds 16 possible values ranging
+ * from 0 (BLACK) to 15 (WHITE).
+ */
+
+struct vga_color {
+	uint8_t color;
+};
+
+#define COLOR_VGA_BLACK			0
+#define COLOR_VGA_BLUE			1
+#define COLOR_VGA_GREEN			2
+#define COLOR_VGA_CYAN			3
+#define COLOR_VGA_RED			4
+#define COLOR_VGA_PURPLE		5
+#define COLOR_VGA_BROWN			6
+#define COLOR_VGA_GRAY			7
+#define COLOR_VGA_DARKGRAY		8
+#define COLOR_VGA_LIGHTBLUE		9
+#define COLOR_VGA_LIGHTGREEN	10
+#define COLOR_VGA_LIGHTCYAN		11
+#define COLOR_VGA_LIGHTRED		12
+#define COLOR_VGA_LIGHTPURPLE	13
+#define COLOR_VGA_YELLOW		14
+#define COLOR_VGA_WHITE			15
+
+#define COLOR_VGA(x)			(struct color){ .vga.color = (x) }
+
+struct color {
+	union {
+		struct rgb_color rgb;
+		struct hex_color hex;
+		struct vga_color vga;
+	};
+};
+
 struct console {
+	uint32_t active;
 	uint32_t fbaddr;
 
 	/*
@@ -35,6 +100,24 @@ struct console {
 	uint32_t xpos;
 	uint32_t ypos;
 	uint32_t bpu;
+
+	/*
+	 * Colors for both foreground and background. Foreground colors only refer
+	 * to the color of fonts while background colors for everything behind and
+	 * between the fonts.
+	 */
+	struct color fgcolor;
+	struct color bgcolor;
+};
+
+struct console_attr {
+	uint32_t active;
+	uint32_t width;
+	uint32_t height;
+	uint32_t xpos;
+	uint32_t ypos;
+	struct color fgcolor;
+	struct color bgcolor;
 };
 
 static inline void *console_unit(struct console *cons, uint32_t xpos, uint32_t ypos)
